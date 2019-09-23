c31ac9e56b2a62d0d4cd79c519140e002d336393
cchudac <cchudac@gmail.com>
2019-09-23 10:09:57 +0200

    Folder hierarchy changed. We now have folder GameObjects, and there are folders Life and NotLife in it. The NotLife folder only Has Projectile class in it.
    I removed all of the junk nugget packages.

--- Pokus1/GameObjects/NotLife/Projectile.cs
+++ Pokus1/GameObjects/NotLife/Projectile.cs
@@ -1,66 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using CoreLib;
+using System.Drawing;
 
 namespace Pokus1
 {
-	public enum AttackSource { enemy, ally, both }
-	public abstract class Attack
-	{
-		public static int DefaultDamage => 10;
-		public Attack(AttackSource source, int attackRange)
-		{
-			this.source = source;
-			this.attackRange = attackRange;
-		}
-		protected int attackRange;
-		public readonly AttackSource source;
-		public abstract void Proceed(Life source);
 
-	}
-
-
-	public class RangedAttack : Attack
-	{
-		readonly Size projectileSize;
-		int projectileSpeed;
-		public RangedAttack(AttackSource source, int attackRange,
-			Size projectileSize, int projectileSpeed)
-			: base (source, attackRange)
-		{
-			this.projectileSize = projectileSize;
-			this.projectileSpeed = projectileSpeed;
-		}
-		public override void Proceed(Life source)
-		{
-			IAnimation animation;
-			if (this.source == AttackSource.ally)
-				animation = new SingleColorAnimation(Color.ForestGreen);
-			else if (this.source == AttackSource.enemy)
-				animation = new SingleColorAnimation(Color.OrangeRed);
-			else if (this.source == AttackSource.both)
-				animation = new SingleColorAnimation(Color.RosyBrown);
-			else throw new Exception("Unknown type (" + this.source.ToString() +
-				") of attackSource is trying to fire a projectile.");
-			source.Map.NoninteractiveItems.Add(
-			new Projectile(
-				this.source,
-				attackRange,
-				source.Middle + ((Location)source.LookingAt * ((Location)source.Size / 2)),
-				projectileSize,
-				source.Name,
-				source.Map,
-				projectileSpeed,
-				source.LookingAt,
-				animation));
-		}
-	}
-
-	public class Projectile: INoninteractiveItem, IMovableObject
+	public class Projectile : INoninteractiveItem, IMovableObject
 	{
 		readonly Location startingLoc;
 		readonly long distance;
@@ -139,19 +89,6 @@ namespace Pokus1
 			}
 			else
 				map.RemoveMe(this);
-		}
-	}
-
-
-	public class MeleeAttack : Attack
-	{
-		public MeleeAttack(AttackSource source, int attackRange) : base(source, attackRange)
-		{
-		}
-
-		public override void Proceed(Life source)
-		{
-			throw new NotImplementedException();
 		}
 	}
 
